591787023651e13951494ba0da8acb6dc1460420
AlexK <amkurochkin@vtb.ru>
2022-06-23 20:01:17 +0600

    generate AATH BackChannel server stub

---
+++ src/main/java/com/sirius/aath/backchannel/AathBackchannelApplication.java
@@ -0,0 +1,13 @@
+package com.sirius.aath.backchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AathBackchannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AathBackchannelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirius/aath/backchannel/AathBackchannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirius.aath.backchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AathBackchannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
